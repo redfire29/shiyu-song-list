c2f769c130d61614628921164e02a51c94a89635
DIANAxAKKO <redfire29@gmail.com>
2025-12-25 11:26:58 +0800

    feat: Enable SSR by removing explicit `ssr: false` configuration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false,
   modules: ['@nuxtjs/tailwindcss'],
   tailwindcss: {
     cssPath: [
