39f4b68258decadde9521d2120d5239762906206
巧克力 <redfire29@gmail.com>
2025-12-26 08:50:15 +0800

    feat: 新增 Nuxt 應用程式的 head 設定並簡化 README 內容

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,29 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/shiyu-song-list/', // 例如 '/my-music-list/'，如果是主域名則用 '/'
     buildAssetsDir: 'assets',
+    head: {
+      htmlAttrs: {
+        lang: 'zh-Hant',
+      },
+      title: 'しゆ。Song List',
+      meta: [
+        { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'apple-mobile-web-app-title', content: '', },
+        { property: 'og:url', content: 'https://redfire29.github.io/shiyu-song-list/' },
+        { property: 'og:site_name', content: 'しゆ。Song List' },
+      ],
+      link: [
+        { rel: 'canonical', href: 'https://redfire29.github.io/shiyu-song-list/' },
+        // { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96', },
+        // { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg', },
+        // { rel: 'shortcut icon', href: '/favicon.ico', },
+        // { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png', },
+        // { rel: 'manifest', href: '/site.webmanifest', },
+      ],
+      script: [],
+    }
   },
   routeRules: {
     '/': { prerender: true }
